c16f7eabd6fde80122c49e7ffd8ea776a78e9c16
Jürgen Fritz <fritz.juergen@gmail.com>
2024-12-03 10:34:19 +0000

    refactored solution day 3

--- Day3/solution.js
+++ Day3/solution.js
@@ -3,27 +3,14 @@ function day3() {
     const fs = require('fs');
     const input = fs.readFileSync('./input.txt', 'utf8');
 
-    const part1Result = part1(input);
-    const part2Result = part2(input);
+    const part1Result = parse(input, /mul\((\d+),(\d+)\)/g);
+    const part2Result = parse(input, /mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g);
 
     console.log('[D3P1]', part1Result);
     console.log('[D3P2]', part2Result);
 
 }
-function part1(input) {
-    const regex = /mul\((\d+),(\d+)\)/g;
-
-    let match;
-    let result = 0;
-
-    while ((match = regex.exec(input)) !== null) {
-        result += parseInt(match[1]) * parseInt(match[2]);
-    }
-    return result;
-}
-function part2(input) {
-
-    const regex = /mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g;
+function parse(input, regex) {
 
     let match;
     let result = 0;
