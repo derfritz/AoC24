716bf344a00d43a74d3a059e65329091b07055d9
Jürgen Fritz <fritz.juergen@gmail.com>
2024-12-05 13:10:09 +0100

    refactoring, use sort to validate and correct
    thx for the lesson niksimon

--- Day5/solution.js
+++ Day5/solution.js
@@ -3,73 +3,36 @@ function day5() {
     const fs = require('fs');
     const input = fs.readFileSync('./input.txt', 'utf8');
     const rows = input.split('\n');
-    const controlMap = new Map();
+    const controlMap = {};
+
+    rows.filter(row => row.includes('|'))
+        .map(row => row.split('|').map(Number))
+        .forEach(([key, value]) => (controlMap[key] ??= []).push(value));
 
     const instructions = rows
         .filter(row => row.includes(','))
         .map(row => row.split(',').map(Number));
 
+    const sortByInstructions = (a, b) => {
+        if (controlMap[a]?.includes(b)) return -1;
+        if (controlMap[b]?.includes(a)) return 1;
+        return 0;
+    }
+    const isValid = (instructions) => {
+        return [...instructions].sort(sortByInstructions).join(',') === instructions.join(',');
+    }
+
     let validCount = 0, repairCount = 0;
 
-    const validInstructions = (instructions) => {
-
-        let valid = true;
-        for (let i = 0; i < instructions.length; i++) {
-
-            const current = instructions[i];
-            const control = controlMap.get(current) || [];
-            const before = instructions.slice(0, i);
-
-            const invalid = control.some(num => before.includes(num));
-            if (invalid) {
-                valid = false;
-            }
+    instructions.forEach(instruction => {
+           isValid(instruction) ?
+               validCount  += instruction[Math.floor(instruction.length / 2)] :
+               repairCount += instruction.sort(sortByInstructions)[Math.floor(instruction.length / 2)];
         }
-        return valid;
-    }
-
-    const repairInstructions = (instructions) => {
-
-        let repaired = [...instructions];
-
-        while (!validInstructions(repaired)) {
-
-            for (let i = 0; i < repaired.length; i++) {
-
-                const current = repaired[i];
-                const control = controlMap.get(current) || [];
-                const before = repaired.slice(0, i);
-
-                if (control.some(num => before.includes(num))) {
-                    for (let j = before.length - 1; j >= 0; j--) {
-                        const num = repaired[j];
-                        if (control.includes(num)) {
-                            repaired[j] = current;
-                            repaired[i] = num;
-                            i = j;
-                        }
-                    }
-                }
-            }
-        }
-        return repaired;
-    }
-
-    rows.filter(row => row.includes('|'))
-        .map(row => row.split('|'))
-        .map(([key, value]) => [Number(key), Number(value)])
-        .forEach(([key, value]) => controlMap.set(key, controlMap.has(key) ? [...controlMap.get(key), value].sort() : [value]));
-
-    instructions
-        .filter(validInstructions)
-        .reduce((acc, val) => validCount += val[Math.floor(val.length / 2)], validCount);
-
-    instructions
-        .filter(i => !validInstructions(i))
-        .map(repairInstructions)
-        .reduce((acc, val) => repairCount += val[Math.floor(val.length / 2)], repairCount);
+    )
 
     console.log('[D5P1]', validCount);
     console.log('[D5P2]', repairCount);
 }
+
 day5();
