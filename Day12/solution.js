d584feca4db763845ac78b1f0a2bafa371e114da
Juergen Fritz <fritz.juergen@gmail.com>
2024-12-13 00:27:56 +0100

    another "optimization" because i cannot give it a rest...
    honestly i liked the first version best, straight to the point, documentation not needed because everything was explicit, and it was faster too

--- Day12/solution.js
+++ Day12/solution.js
@@ -9,24 +9,21 @@ function day12() {
         // n       e       w        s        ne       nw       sw       se
         [-1, 0], [0, 1], [0, -1], [1, 0], [-1, 1], [-1, -1], [1, -1], [1, 1]
     ];
-
+    const getNeighbors = (pos) => directions.map(([dx, dy]) => charAt( [pos[0] + dx, pos[1] + dy]) === charAt(pos));
     const withinMatrix = (pos) => pos[0] >= 0 && pos[0] < matrix.length && pos[1] >= 0 && pos[1] < matrix[pos[0]].length;
     const charAt = (pos) => pos && withinMatrix(pos) ? matrix[pos[0]][pos[1]] : '';
+    const toKey = (pos) => `${pos[0]}-${pos[1]}`;
     const walk = (pos, previous, plan) => {
 
         const [x, y] = pos;
         const key = `${x}-${y}`;
         const char = charAt(pos);
 
-        if (previous && charAt(previous) !== charAt(pos)) return; // out of bounds
+        if (previous && charAt(previous) !== char) return; // out of bounds
         if (visited.has(key)) return; // already visited
 
-        visited.add(key);
-
         // get neighbors: if they are the same as the current char -> true
-        const [n, e, w, s, ne, nw, sw, se] =
-            directions.map(([dx, dy]) => charAt([x + dx, y + dy]) === char);
-
+        const [n, e, w, s, ne, nw, sw, se] = getNeighbors(pos);
         // check fences
         const fences = [n, e, w, s].filter(b => !b).length;
         // check corners
@@ -38,9 +35,11 @@ function day12() {
         ].filter(Boolean).length;
 
         plan.set(key, {corners, fences});
+        visited.add(key);
 
-        return  walk([x + 1, y], pos, plan) || walk([x - 1, y], pos, plan) ||
-                walk([x, y + 1], pos, plan) || walk([x, y - 1], pos, plan);
+        // walk: n, e, w, s
+        [directions[0], directions[1], directions[2], directions[3]]
+            .forEach(([dx, dy]) => {walk([x + dx, y + dy], pos, plan)});
     }
     const plot = (pos) => {
         const plan = new Map();
@@ -63,7 +62,7 @@ function day12() {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
 
-            if (!visited.has(`${i}-${j}`)) {
+            if (!visited.has(toKey([i, j]))) {
                 const estimate = plot([i, j]);
                 cost += estimate.size * estimate.fences;
                 costDeducted += estimate.size * estimate.corners;
